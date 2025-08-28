2fc1ad43a8db357a247604379e6530547fca109c
helljh <junho308917@naver.com>
2025-08-28 15:20:03 +0900

    Feat: 음성일기 작성 Repository 작성

---
+++ GrowIT/Feat/Diary/Data/Repository/DiaryVoiceRepositoryImpl.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryRepositoryImpl.swift
+//  GrowIT
+//
+//  Created by 허준호 on 8/16/25.
+//
+
+class DiaryVoiceRepositoryImpl: DiaryVoiceRepository {
+    private let dataSource: DiaryVoiceDataSource
+    
+    init(dataSource: DiaryVoiceDataSource) {
+        self.dataSource = dataSource
+    }
+    
+    func postVoiceDiary(chat: String) async throws -> String {
+        let data = DiaryVoiceDTO(chat: chat)
+        let response = try await dataSource.postVoiceDiary(data: data)
+        return response.chat
+    }
+}
