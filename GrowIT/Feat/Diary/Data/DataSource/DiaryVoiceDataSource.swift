28aa8a79309083e3bd2a6f6efc183d1afafcc630
helljh <junho308917@naver.com>
2025-08-28 15:18:41 +0900

    Feat: 음성일기 작성 DataSource 작성

---
+++ GrowIT/Feat/Diary/Data/DataSource/DiaryVoiceDataSource.swift
@@ -0,0 +1,32 @@
+//
+//  DiaryVoiceDataSource.swift
+//  GrowIT
+//
+//  Created by 허준호 on 8/16/25.
+//
+
+protocol DiaryVoiceDataSource {
+    func postVoiceDiary(data: DiaryVoiceDTO) async throws -> DiaryVoiceDTO
+}
+
+class DiaryVoiceDataSourceImpl: DiaryVoiceDataSource {
+    private let diaryService: DiaryService
+    
+    init(diaryService: DiaryService) {
+        self.diaryService = diaryService
+    }
+    
+    func postVoiceDiary(data: DiaryVoiceDTO) async throws -> DiaryVoiceDTO {
+        return try await withCheckedThrowingContinuation { continuation in
+           diaryService.postVoiceDiary(data: data) { result in
+               switch result {
+               case .success(let data):
+                   continuation.resume(returning: data)
+               case .failure(let error):
+                   print("서버에 음성대화 보내기 실패")
+                   continuation.resume(throwing: error)
+               }
+           }
+       }
+    }
+}
