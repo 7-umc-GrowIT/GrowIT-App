598d23278734f474ea56bf86568fce1c925f43d1
hyunm1n-o <hmoh0703@naver.com>
2025-09-04 15:54:41 +0900

    Feat: 홈 로딩 인디케이터 추가

--- GrowIT/Feat/Home/Presentation/ViewController/HomeViewController.swift
+++ GrowIT/Feat/Home/Presentation/ViewController/HomeViewController.swift
@@ -28,6 +28,7 @@ class HomeViewController: UIViewController {
         self.view = homeview
         loadGroImage()
         setNotification()
+//        homeview.characterArea.showLoadingIndicator()
     }
     
     override func viewDidLayoutSubviews() {
