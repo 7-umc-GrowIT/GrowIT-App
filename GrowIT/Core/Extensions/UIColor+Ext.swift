e8aef39aab37f9759340f7af53837d75e89d3a08
helljh <junho308917@naver.com>
2025-09-15 14:19:33 +0900

    Fix: 감정 키워드 배경색 다크모드 적용

--- GrowIT/Core/Extensions/UIColor+Ext.swift
+++ GrowIT/Core/Extensions/UIColor+Ext.swift
@@ -87,3 +87,23 @@ extension UIColor{
         self.init(red: r, green: g, blue: b, alpha: a)
     }
 }
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted.remove(at: hexFormatted.startIndex)
+        }
+
+        assert(hexFormatted.count == 6, "Invalid hex code")
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        let r = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let g = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let b = CGFloat(rgbValue & 0x0000FF) / 255.0
+
+        self.init(red: r, green: g, blue: b, alpha: alpha)
+    }
+}
